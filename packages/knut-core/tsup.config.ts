0c8b6df594a20a6bc8508b93fb720bb1b093afe2
Jared Rickert <jaredrickert52@gmail.com>
2023-12-13 11:43:11 -0600

    build(core): add build system
    Add a build system with tsup.

---
+++ packages/knut-core/tsup.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig, type Options } from 'tsup';
+import path from 'path';
+
+const env = process.env.NODE_ENV;
+const isProduction = env === 'production';
+
+const commonConfig: Options = {
+	banner: {
+		// js: "'use client'"
+	},
+	clean: true,
+	sourcemap: isProduction,
+	format: ['cjs', 'esm'],
+	bundle: isProduction,
+	minify: isProduction,
+	dts: true,
+	treeshake: true,
+	external: ['react'],
+	splitting: true,
+	target: 'es2020',
+	outDir: 'dist',
+	tsconfig: path.resolve(__dirname, './tsconfig.json'),
+	skipNodeModulesBundle: true,
+};
+
+export default defineConfig(() => {
+	return [
+		{
+			entry: ['./src/index.ts'],
+			...commonConfig,
+			esbuildOptions: (options) => {
+				options.outbase = 'src';
+			},
+			// the index doesn't need to be bundled
+			bundle: false,
+		},
+		{
+			entry: ['./src/**/!(index).ts'],
+			...commonConfig,
+			esbuildOptions: (options) => {
+				options.outbase = 'src';
+			},
+		},
+	];
+});
