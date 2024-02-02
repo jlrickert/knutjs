5e679ad2f5c34d219eed49e7c8427a8461afa45d
Jared Rickert <jaredrickert52@gmail.com>
2024-02-02 10:50:49 -0600

    feat(cli): add update command
    Add update command to the CLI that just calls update on knut

--- packages/knut-cli/src/cli.ts
+++ packages/knut-cli/src/cli.ts
@@ -1,58 +1,14 @@
 import { configCli } from './configCli.js';
-import { share } from './index.js';
+import { knutCli } from './knutCli.js';
 import { kegCli } from './kegCli.js';
-import { KnutCommand, knutCli } from './knutCli.js';
 import { searchCli } from './searchCli.js';
+import { shareCli } from './shareCli.js';
+import { updateCli } from './updateCli.js';
 
 knutCli.addCommand(searchCli);
 knutCli.addCommand(kegCli);
-
-// const kegCli = KegCommand('keg').addCommand(
-// 	new Command('directory').action((options, parent: Command) => {
-// 		console.log({ message: 'ok', options, parent: parent });
-// 	}),
-// );
-// knutCli.addCommand(kegCli);
-
-// const nodeCli = KegCommand('node')
-// 	.addOption(kegpathOption)
-// 	.addCommand(
-// 		new Command('list').action((a, b) => {
-// 			console.log({ a, b });
-// 		}),
-// 	)
-// 	.addCommand(new Command('last'));
-// knutCli.addCommand(nodeCli);
-
-// export const indexCli = KegCommand('index')
-// 	.action((options, command: Command) => {
-// 		console.log({ options, parent: command.parent?.opts() });
-// 		log('index');
-// 	})
-// 	.addHelpText('beforeAll', 'update index')
-// 	.enablePositionalOptions(true)
-// 	.passThroughOptions(true)
-// 	.addCommand(
-// 		new Command('update')
-// 			.enablePositionalOptions(true)
-// 			.passThroughOptions(true)
-// 			.action((options, command: Command) => {
-// 				console.log({
-// 					options,
-// 					root: command.parent?.parent?.opts(),
-// 					parent: command.parent?.opts(),
-// 				});
-// 			}),
-// 	);
-// knutCli.addCommand(indexCli);
-
-const shareCli = KnutCommand('share')
-	.argument('<node_id>')
-	.addHelpText('before', 'Shares a node on s3 and provides a temporary link')
-	.action((nodeId, options) => {
-		share(nodeId, options);
-	});
 knutCli.addCommand(shareCli);
 knutCli.addCommand(configCli);
+knutCli.addCommand(updateCli);
 
 export { knutCli };
